755490c0bf450af4fac4833bdd5100450f54b0d1
Nicholas Silas <njsilas@gmail.com>
2020-11-01 13:30:08 -0500

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,25 @@
-# Your code goes here!
+require "pry"
+
+
+class Anagram
+
+    attr_accessor :words
+    
+    
+    def initialize(words)
+        @words = words
+        
+    
+    end
+    
+    def match(outside)
+        nu_a = []
+        outside.collect do |word|
+            if word.split("").sort == @words.split("").sort
+                nu_a << word
+            end
+       end
+    nu_a
+    end
+   
+end
